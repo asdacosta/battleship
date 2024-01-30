719fe186d0381283a1d0e142bb05e49eaea8f93a
asdacosta <asdacosta18@gmail.com>
2024-01-30 11:06:36 +0000

    Test isSunk method

--- src/index.test.js
+++ src/index.test.js
@@ -4,13 +4,21 @@ test("what instances of Ship contain", () => {
   const ship1 = new Ship(3, 0, false);
 
   expect(ship1).toBeDefined();
-  expect(ship1).toEqual({ length: 3, hits: 0, sunk: false });
+  expect(ship1).toEqual({ length: 3, numHits: 0, sunk: false });
 });
 
 test("hit me until I'm sunk", () => {
   const submarine = new Ship(1, 0, false);
   submarine.hit();
 
-  expect(submarine.shipHits).toBe(1);
+  expect(submarine.currentNumHits).toBe(1);
   expect(submarine.hit()).toBe("Ship already sunk!");
+});
+
+test("check if ship is sunk", () => {
+  const sub = new Ship(1, 0, false);
+
+  expect(sub.isSunk()).toBe(false);
+  sub.hit();
+  expect(sub.isSunk()).toBe(true);
 });
