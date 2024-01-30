899be906e64a7d53d9af4b5bddf998154db08c8e
asdacosta <asdacosta18@gmail.com>
2024-01-30 00:00:17 +0000

    Change to dev mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,44 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  // mode: 'development',
-  // devtool: 'inline-source-map',
-  mode: 'production',
-  devtool: 'source-map',
-  entry: './src/index.js',
+  mode: "development",
+  devtool: "inline-source-map",
+  // mode: 'production',
+  // devtool: 'source-map',
+  entry: "./src/index.js",
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
     clean: true,
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
         generator: {
-          filename: 'assets/[name][ext]',
+          filename: "assets/[name][ext]",
         },
       },
       {
         test: /\.(mp4|webm)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
         generator: {
-          filename: 'assets/[name][ext]',
+          filename: "assets/[name][ext]",
         },
       },
       {
         test: /\.(?:js|mjs|cjs)$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: [['@babel/preset-env', { targets: 'defaults' }]],
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
           },
         },
       },
@@ -46,13 +46,13 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html',
-      filename: 'index.html',
-      inject: 'head',
-      scriptLoading: 'defer',
+      template: "./src/index.html",
+      filename: "index.html",
+      inject: "head",
+      scriptLoading: "defer",
     }),
   ],
   devServer: {
-    static: './src',
+    static: "./src",
   },
 };
