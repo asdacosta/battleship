57221ebb597aeeec61942bd6ff01604d9a08a40a
asdacosta <asdacosta18@gmail.com>
2024-02-03 07:22:24 +0000

    Populate board with horizontal ships and update legal moves on board by occupying non-free spots with 'O'

--- src/index.js
+++ src/index.js
@@ -75,6 +75,7 @@ class Gameboard {
       for (let m = 0; m < length; m++) {
         firstMove.push(m);
       }
+      // Account for extra moves for subsequent ships
       switch (length) {
         case 4:
           length += 2;
@@ -85,6 +86,9 @@ class Gameboard {
         case 2:
           length += 6;
           break;
+        case 1:
+          length += 8;
+          break;
       }
       for (let n = 0; n <= length; n++) {
         shipMoves.push(firstMove);
@@ -94,11 +98,11 @@ class Gameboard {
       return shipMoves;
     };
 
-    const carrierMoves = generateMoves(5);
-    const battleshipMoves = generateMoves(4);
-    const destroyerMoves = generateMoves(3);
-    const submarineMoves = generateMoves(3);
-    const patrolMoves = generateMoves(2);
+    const carrierMoves = generateMoves(this.ships.Carrier.length);
+    const battleshipMoves = generateMoves(this.ships.Battleship.length);
+    const destroyerMoves = generateMoves(this.ships.Destroyer.length);
+    const submarineMoves = generateMoves(this.ships.Submarine.length);
+    const patrolMoves = generateMoves(this.ships["Patrol Boat"].length);
 
     const legalMoves = [
       carrierMoves,
@@ -110,9 +114,151 @@ class Gameboard {
 
     return legalMoves;
   }
+
+  displaceShips() {
+    const board = this.createBoard();
+
+    const generateRandomRowIndex = function () {
+      const rowIndex = Math.floor(Math.random() * 10);
+      return rowIndex;
+    };
+
+    const getRandomMoveIndex = function (moves) {
+      const moveIndex = Math.floor(Math.random() * moves);
+      return moveIndex;
+    };
+
+    const updateLegalMovesInBoard = function (rowIndex, shipLength) {
+      const populatedRow = board[rowIndex];
+      const lastOccupied = populatedRow.lastIndexOf(shipLength);
+      let firstOccupied = null;
+
+      const occupy = function (
+        firstIndexEmpty,
+        lastIndexEmpty,
+        firstTopBottom,
+        lastTopBottom,
+      ) {
+        // Occupy first and last index of ship
+        firstOccupied = populatedRow.indexOf(shipLength);
+        if (firstIndexEmpty && !lastIndexEmpty) {
+          populatedRow[firstOccupied - 1] = "O";
+        } else if (!firstIndexEmpty && lastIndexEmpty) {
+          populatedRow[lastOccupied + 1] = "O";
+        } else if (firstIndexEmpty && lastIndexEmpty) {
+          populatedRow[firstOccupied - 1] = "O";
+          populatedRow[lastOccupied + 1] = "O";
+        }
+        // Occupy top and/or bottom
+        if (rowIndex === 0) {
+          const bottomAdjacentRow = board[1];
+          bottomAdjacentRow.fill(
+            "O",
+            firstOccupied - firstTopBottom,
+            lastOccupied + lastTopBottom,
+          );
+        } else if (rowIndex === 9) {
+          const topAdjacentRow = board[8];
+          topAdjacentRow.fill(
+            "O",
+            firstOccupied - firstTopBottom,
+            lastOccupied + lastTopBottom,
+          );
+        } else {
+          const topAdjacentRow = board[rowIndex - 1];
+          const bottomAdjacentRow = board[rowIndex + 1];
+          topAdjacentRow.fill(
+            "O",
+            firstOccupied - firstTopBottom,
+            lastOccupied + lastTopBottom,
+          );
+          bottomAdjacentRow.fill(
+            "O",
+            firstOccupied - firstTopBottom,
+            lastOccupied + lastTopBottom,
+          );
+        }
+      };
+      if (
+        populatedRow[0] === null &&
+        populatedRow[populatedRow.length - 1] !== null &&
+        !populatedRow.includes("O")
+      ) {
+        occupy(true, false, 1, 1);
+      } else if (
+        populatedRow[0] !== null &&
+        populatedRow[populatedRow.length - 1] === null &&
+        !populatedRow.includes("O")
+      ) {
+        occupy(false, true, 0, 2);
+      } else if (
+        populatedRow[0] === null &&
+        populatedRow[populatedRow.length - 1] === null &&
+        !populatedRow.includes("O")
+      ) {
+        occupy(true, true, 1, 2);
+      }
+    };
+
+    const populateBoard = (() => {
+      const legalMoves = this.getLegalMoves();
+
+      const _withSpecifiedShip = (ship, index) => {
+        const randomRowIndex = generateRandomRowIndex();
+        const shipMoves = legalMoves[index];
+        let shipLength = null;
+
+        switch (ship) {
+          case "Carrier":
+            shipLength = this.ships.Carrier.length;
+            break;
+          case "Battleship":
+            shipLength = this.ships.Battleship.length;
+            break;
+          case "Destroyer":
+            shipLength = this.ships.Destroyer.length;
+            break;
+          case "Submarine":
+            shipLength = this.ships.Submarine.length;
+            break;
+          case "Patrol Boat":
+            shipLength = this.ships["Patrol Boat"].length;
+            break;
+        }
+
+        const randomShipMove = getRandomMoveIndex(shipMoves.length);
+        const firstShipMove = shipMoves[randomShipMove][0];
+        const shipMoveLastIndex = shipMoves[randomShipMove].length - 1;
+        const lastShipMove = shipMoves[randomShipMove][shipMoveLastIndex];
+
+        board.forEach((row, rowIndex) => {
+          if (rowIndex === randomRowIndex) {
+            // Always occupy empty row
+            while (row.includes("O")) {
+              _withSpecifiedShip(ship, index);
+              return;
+            }
+            row.fill(shipLength, firstShipMove, lastShipMove + 1);
+          }
+        });
+        updateLegalMovesInBoard(randomRowIndex, shipLength);
+      };
+
+      _withSpecifiedShip("Carrier", 0);
+      _withSpecifiedShip("Battleship", 1);
+      _withSpecifiedShip("Destroyer", 2);
+    })();
+
+    console.log(board);
+    return board;
+    //TODO: Left over board and available moves
+  }
 }
 
 module.exports = {
   Ship,
   Gameboard,
 };
+
+// const everBoard = new Gameboard();
+// everBoard.displaceShips();
