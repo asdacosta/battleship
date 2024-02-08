cd9ccdfd02a9825b852d6beaaa085fc0ff37d2af
asdacosta <asdacosta18@gmail.com>
2024-02-08 07:20:06 +0000

    Import assets and stylesheets

---
+++ src/battleground.js
@@ -0,0 +1,10 @@
+import "./reset.css";
+import "./battleground.css";
+
+const importAllAssets = (function () {
+  function importAll(r) {
+    return r.keys().map(r);
+  }
+
+  const assets = importAll(require.context("./assets", false, /\.(png|jpe?g|svg)$/));
+})();
