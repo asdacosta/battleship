7ca92787abf9c568c1c0a359936578f0e58493d3
asdacosta <asdacosta18@gmail.com>
2024-03-13 19:26:03 +0000

    Use an svg to indicate ship sunk instead of emoji

--- src/battleground.js
+++ src/battleground.js
@@ -517,31 +517,36 @@ const loopGame = (function () {
         case "5":
           feedbackMessage = "Hit the Carrier 🎯";
           if (victim.ships.Carrier.currentNumHits === 5) {
-            feedbackMessage = "Sunk the Carrier! 🌟";
+            feedbackMessage =
+              'Sunk the Carrier! <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>sail-boat-sink</title><path d="M20.96 21C19.9 21 18.9 20.74 17.96 20.24C16.12 21.24 13.81 21.24 11.97 20.24C10.13 21.24 7.82 21.24 6 20.24C4.77 20.93 3.36 21.04 2 21V19C3.41 19.04 4.77 18.89 6 18C7.74 19.24 10.21 19.24 11.97 18C13.74 19.24 16.2 19.24 17.96 18C19.17 18.89 20.54 19.04 21.94 19V21H20.96M22 3.5L7.11 5.96L13.11 12.17L22 3.5M10.81 16.36L11.97 15.54L13.12 16.36C13.65 16.72 14.3 16.93 14.97 16.93C15.12 16.93 15.28 16.91 15.43 16.89L5.2 6.31C4.29 7.65 3.9 9.32 4 10.92L9.74 16.83C10.13 16.74 10.5 16.58 10.81 16.36Z" /></svg>';
           }
           break;
         case "4":
           feedbackMessage = "Hit the Battleship 🎯";
           if (victim.ships.Battleship.currentNumHits === 4) {
-            feedbackMessage = "Sunk the Battleship! 🌟";
+            feedbackMessage =
+              'Sunk the Battleship! <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>sail-boat-sink</title><path d="M20.96 21C19.9 21 18.9 20.74 17.96 20.24C16.12 21.24 13.81 21.24 11.97 20.24C10.13 21.24 7.82 21.24 6 20.24C4.77 20.93 3.36 21.04 2 21V19C3.41 19.04 4.77 18.89 6 18C7.74 19.24 10.21 19.24 11.97 18C13.74 19.24 16.2 19.24 17.96 18C19.17 18.89 20.54 19.04 21.94 19V21H20.96M22 3.5L7.11 5.96L13.11 12.17L22 3.5M10.81 16.36L11.97 15.54L13.12 16.36C13.65 16.72 14.3 16.93 14.97 16.93C15.12 16.93 15.28 16.91 15.43 16.89L5.2 6.31C4.29 7.65 3.9 9.32 4 10.92L9.74 16.83C10.13 16.74 10.5 16.58 10.81 16.36Z" /></svg>';
           }
           break;
         case "3.5":
           feedbackMessage = "Hit the Destroyer 🎯";
           if (victim.ships.Destroyer.currentNumHits === 3) {
-            feedbackMessage = "Sunk the Destroyer! 🌟";
+            feedbackMessage =
+              'Sunk the Destroyer! <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>sail-boat-sink</title><path d="M20.96 21C19.9 21 18.9 20.74 17.96 20.24C16.12 21.24 13.81 21.24 11.97 20.24C10.13 21.24 7.82 21.24 6 20.24C4.77 20.93 3.36 21.04 2 21V19C3.41 19.04 4.77 18.89 6 18C7.74 19.24 10.21 19.24 11.97 18C13.74 19.24 16.2 19.24 17.96 18C19.17 18.89 20.54 19.04 21.94 19V21H20.96M22 3.5L7.11 5.96L13.11 12.17L22 3.5M10.81 16.36L11.97 15.54L13.12 16.36C13.65 16.72 14.3 16.93 14.97 16.93C15.12 16.93 15.28 16.91 15.43 16.89L5.2 6.31C4.29 7.65 3.9 9.32 4 10.92L9.74 16.83C10.13 16.74 10.5 16.58 10.81 16.36Z" /></svg>';
           }
           break;
         case "3":
           feedbackMessage = "Hit the Submarine 🎯";
           if (victim.ships.Submarine.currentNumHits === 3) {
-            feedbackMessage = "Sunk the Submarine! 🌟";
+            feedbackMessage =
+              'Sunk the Submarine! <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>sail-boat-sink</title><path d="M20.96 21C19.9 21 18.9 20.74 17.96 20.24C16.12 21.24 13.81 21.24 11.97 20.24C10.13 21.24 7.82 21.24 6 20.24C4.77 20.93 3.36 21.04 2 21V19C3.41 19.04 4.77 18.89 6 18C7.74 19.24 10.21 19.24 11.97 18C13.74 19.24 16.2 19.24 17.96 18C19.17 18.89 20.54 19.04 21.94 19V21H20.96M22 3.5L7.11 5.96L13.11 12.17L22 3.5M10.81 16.36L11.97 15.54L13.12 16.36C13.65 16.72 14.3 16.93 14.97 16.93C15.12 16.93 15.28 16.91 15.43 16.89L5.2 6.31C4.29 7.65 3.9 9.32 4 10.92L9.74 16.83C10.13 16.74 10.5 16.58 10.81 16.36Z" /></svg>';
           }
           break;
         case "2":
           feedbackMessage = "Hit the Patrol Boat 🎯";
           if (victim.ships["Patrol Boat"].currentNumHits === 2) {
-            feedbackMessage = "Sunk the Patrol Boat! 🌟";
+            feedbackMessage =
+              'Sunk the Patrol Boat! <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>sail-boat-sink</title><path d="M20.96 21C19.9 21 18.9 20.74 17.96 20.24C16.12 21.24 13.81 21.24 11.97 20.24C10.13 21.24 7.82 21.24 6 20.24C4.77 20.93 3.36 21.04 2 21V19C3.41 19.04 4.77 18.89 6 18C7.74 19.24 10.21 19.24 11.97 18C13.74 19.24 16.2 19.24 17.96 18C19.17 18.89 20.54 19.04 21.94 19V21H20.96M22 3.5L7.11 5.96L13.11 12.17L22 3.5M10.81 16.36L11.97 15.54L13.12 16.36C13.65 16.72 14.3 16.93 14.97 16.93C15.12 16.93 15.28 16.91 15.43 16.89L5.2 6.31C4.29 7.65 3.9 9.32 4 10.92L9.74 16.83C10.13 16.74 10.5 16.58 10.81 16.36Z" /></svg>';
           }
           break;
       }
@@ -583,7 +588,7 @@ const loopGame = (function () {
       }
       if (missedOrHit === "hit") {
         const feedbackMessage = getFeedbackMessage("ai");
-        getNodes.feedback.textContent = `AI: ${feedbackMessage}`;
+        getNodes.feedback.innerHTML = `AI: ${feedbackMessage}`;
       }
     }
 
@@ -593,7 +598,7 @@ const loopGame = (function () {
       }
       if (missedOrHit === "hit") {
         const feedbackMessage = getFeedbackMessage("user");
-        getNodes.feedback.textContent = `Admiral ${retrieveAdmiralNameFromStorageAndSet.admiralName}: ${feedbackMessage}`;
+        getNodes.feedback.innerHTML = `Admiral ${retrieveAdmiralNameFromStorageAndSet.admiralName}: ${feedbackMessage}`;
       }
     }
   };
